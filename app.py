545ccaa13ba10ef598325e050bf40077eb855d4d
AdamAhmed334 <jdd28675@gmail.com>
2026-06-10 04:50:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 from groq import Groq
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
-st.title("MyChatbot")
-if "messages" is not st.session_state:
+st.title("MY CHATBOT")
+
+if "messages" not in st.session_state:
   st.session_state.messages = []
+
 for message in st.session_state.messages:
   with st.chat_message(message["role"]):
     st.write(message["content"])
-prompt_text = st.chat_input("اكتب الرسلة")
+
+prompt_text = st.chat_input("Write your message...")
+
 if prompt_text:
-  st.session_state.messages.append({"role" : "user" , "content" : prompt_text})
+  st.session_state.messages.append({"role" : "user", "content" : prompt_text})
   with st.chat_message("user"):
     st.write(prompt_text)
+
+
+
+
+
+
+
+
+
+
